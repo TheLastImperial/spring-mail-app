aa8980532658c153d17f261cca58f56a6e71c597
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2026-08-26 11:13:37 -0700

    Helper Project created.

---
+++ src/helper/src/main/java/com/thelastimperial/mail/helper/services/ProduceMailService.java
@@ -0,0 +1,8 @@
+package com.thelastimperial.mail.helper.services;
+
+import java.util.function.Consumer;
+
+import com.thelastimperial.mail.helper.requests.MailRequest;
+
+public interface ProduceMailService extends Consumer<MailRequest>{
+}
