f174580b2b96f5b2166b5721681b87136c968396
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2026-09-02 10:14:43 -0700

    Added support to Kafka. Now Can choice one MQ with the active profile.

--- src/mail/src/main/java/com/thelastimperial/mail/mail/services/impl/RabbitProduceMailServiceImpl.java
+++ src/mail/src/main/java/com/thelastimperial/mail/mail/services/impl/RabbitProduceMailServiceImpl.java
@@ -2,6 +2,7 @@ package com.thelastimperial.mail.mail.services.impl;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import com.thelastimperial.mail.helper.requests.MailRequest;
@@ -13,7 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 @Service
 @Slf4j
-public class ProduceMailServiceImpl implements ProduceMailService {
+@Profile("rabbit")
+public class RabbitProduceMailServiceImpl implements ProduceMailService {
     private final RabbitTemplate rabbitTemplate;
     private final Queue queue;
 
